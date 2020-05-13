ce3ec24c1b164231ba3f8bb66c35a2bfb6179050
Mathieu <mathieu.prevot.91@gmail.com>
2020-05-13 17:35:32 +0200

    GameManager => steps + add tilemap

--- GoldMobile/Assets/Script/UI/GameManager.cs
+++ GoldMobile/Assets/Script/UI/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour
     public GhostManager gh;
 
     public float playerSpeed;
+    public int incrementStep = 0;
     private void Awake()
     {
         controleP1 = true;
@@ -43,6 +44,88 @@ public class GameManager : MonoBehaviour
     public void openStep()
     {
         firstRoom = true;
+        if (incrementStep == 0)
+        {
+            //Exterieur
+            //cinematique
+            //chambre mother, sous sol 2, serre & librairy fermés
+
+            //incrementStep++;
+        }
+
+        if (incrementStep == 1)
+        {
+            //main hall
+            //lumière on + déplacement libre joueur
+            //dialogue postIntro
+            // dialogue chambre fille
+            //incrementStep++;
+
+            //chambre girl
+            //wait invocation 
+            //tuto sel
+            //lumière off && invoc petite fille + halo de lumière
+
+            //sous sol 1
+            //secateur
+            //flashBack / vision de fantome
+
+            //Lily chambre
+            //interaction => tapis+latte
+            //boite code 
+
+            //librairy closed, mother room closed 
+
+            //incrementStep++;
+
+        }
+
+        if (incrementStep == 2)
+        {
+            //serre (lié à chambre)
+            //cinématique lié bout de verre
+            //using sécateur => clé + journal
+            //code sur pilier
+
+            //incrementStep++;
+        }
+
+        if (incrementStep == 3)
+        {
+            //library
+            //labyrinthe
+            //wait for doors 
+            //clé to mother room
+
+            //incrementStep++;
+
+        }
+
+        if (incrementStep == 4)
+        {
+            //mother room
+            //pages livre
+            //cinématique
+            //outil to trappe du sous sol
+
+            //incrementStep++;
+
+        }
+
+        if (incrementStep == 5)
+        {
+            //sous sol 2 fond de la librairy
+            //R sauf cinématique
+
+            //incrementStep++;
+
+        }
+
+        if (incrementStep == 6)
+        {
+            //end
+        }
+
 
     }
 
