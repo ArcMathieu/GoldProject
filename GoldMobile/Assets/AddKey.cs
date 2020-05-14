9eba5dc4dec0751826d16e307ff27f1b6e24ed86
Wiloux <54811449+Wiloux@users.noreply.github.com>
2020-05-14 15:37:28 +0200

    KeySystem, isleep

---
+++ GoldMobile/Assets/AddKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddKey : MonoBehaviour
+{
+
+    public string DesiredKey;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<KeySystem>().Keys.Add(DesiredKey);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GoldMobile/Assets/KeySystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySystem : MonoBehaviour
+{
+    public List<string> Keys;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
