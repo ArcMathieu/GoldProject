96a757fdadf3cf6bfde8245fbda9fe5aa463783e
Pitter <ikevin77@hotmail.fr>
2020-05-14 11:11:29 +0200

    cadena

---
+++ GoldMobile/Assets/Script/cadena.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cadena : MonoBehaviour
+{
+    public GameObject number1;
+    public GameObject number2;
+    public GameObject number3;
+    public List<int> pasword;
+    public bool noRepeat = false;
+
+    public void numberUp(GameObject number)
+    {
+        if(number.GetComponent<currentNumber>().number + 1 > 9)
+        {
+            number.GetComponent<currentNumber>().number = 0;
+        }
+        else
+        {
+            number.GetComponent<currentNumber>().number++;
+        }
+
+    }
+
+    public void numberDown(GameObject number)
+    {
+        if (number.GetComponent<currentNumber>().number - 1 < 0)
+        {
+            number.GetComponent<currentNumber>().number = 9;
+        }
+        else
+        {
+            number.GetComponent<currentNumber>().number--;
+        }
+    }
+
+    private void Start()
+    {
+    
+    }
+
+    private void Update()
+    {
+       if(!noRepeat)
+       {
+            if (pasword[0] == number1.GetComponent<currentNumber>().number)
+            {
+                if (pasword[1] == number2.GetComponent<currentNumber>().number)
+                {
+                    if (pasword[2] == number3.GetComponent<currentNumber>().number)
+                    {
+                        Debug.Log("coucou");
+                        noRepeat = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GoldMobile/Assets/Script/currentNumber.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class currentNumber : MonoBehaviour
+{
+
+    public int number;
+    public TextMeshProUGUI text;
+
+    public void Start()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        text.text = "" + number;
+
+    }
+
+
+
+}
