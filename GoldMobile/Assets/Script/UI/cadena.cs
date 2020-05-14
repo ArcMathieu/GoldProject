a2cdb2246ede50447ca58473e431bf9be4d6586b
Pitter <ikevin77@hotmail.fr>
2020-05-14 18:17:05 +0200

    SFX

--- GoldMobile/Assets/Script/UI/cadena.cs
+++ GoldMobile/Assets/Script/UI/cadena.cs
@@ -8,8 +8,9 @@ public class cadena : MonoBehaviour
     public GameObject number1;
     public GameObject number2;
     public GameObject number3;
+    public GameObject Cadena;
     public List<int> pasword;
-    public bool noRepeat = false;
+    public bool noRepeat;
 
     public void numberUp(GameObject number)
     {
@@ -54,6 +55,7 @@ public class cadena : MonoBehaviour
                     if (pasword[2] == number3.GetComponent<currentNumber>().number)
                     {
                         Debug.Log("coucou");
+                        //Cadena.SetActive(false);
                         noRepeat = true;
                     }
                 }
