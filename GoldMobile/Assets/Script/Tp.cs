55a6580f8aff4a66b7c00e82ba009123e395d29c
Mathieu <mathieu.prevot.91@gmail.com>
2020-05-14 15:01:32 +0200

    LD, TP

---
+++ GoldMobile/Assets/Script/Tp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tp : MonoBehaviour
+{
+    public GameObject tpTo;
+    public bool canPass = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && canPass)
+        {
+            collision.transform.position = new Vector2(tpTo.transform.position.x, tpTo.transform.position.y - 3);
+
+            tpTo.SendMessage("CoroutToWait");
+        }
+    }
+
+    public void CoroutToWait()
+    {
+        StartCoroutine(WaitToPass());
+        IEnumerator WaitToPass()
+        {
+            canPass = false;
+            yield return new WaitForSeconds(1.5f);
+            canPass = true;
+        }
+    }
+}
