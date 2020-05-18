60b88e5ff0a0231243a7a2b931c823dd0ebf5846
Pitter <ikevin77@hotmail.fr>
2020-05-18 19:02:57 +0200

    KeyProject + test

--- GoldMobile/Assets/Script/Achievement/GoldM_Authentification.cs
+++ GoldMobile/Assets/Script/Achievement/GoldM_Authentification.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
 
 public class GoldM_Authentification : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static PlayGamesPlatform platform;
+
     void Start()
     {
-        
+        if(platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+
+            platform = PlayGamesPlatform.Activate();
+        }
+
+        Social.Active.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                Debug.Log("Logged successfuly");
+            }
+            else
+            {
+                Debug.Log("Failled");
+            }
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
