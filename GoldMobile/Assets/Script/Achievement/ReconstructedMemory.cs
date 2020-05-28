cd14b48d9325236a973394817dcdfd11af6fedd1
Pitter <ikevin77@hotmail.fr>
2020-05-28 18:34:11 +0200

    dzqdzq

---
+++ GoldMobile/Assets/Script/Achievement/ReconstructedMemory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReconstructedMemory : MonoBehaviour
+{
+
+    public void Start()
+    {
+        FindObjectOfType<Achievement>().UnlockReconstructedMemory();
+    }
+}
