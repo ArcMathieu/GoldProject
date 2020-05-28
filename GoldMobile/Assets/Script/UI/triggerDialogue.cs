625885b37d9c383b5388bedd57c93524036250dc
Mathieu <mathieu.prevot.91@gmail.com>
2020-05-28 19:01:01 +0200

    Maj avant build

--- GoldMobile/Assets/Script/UI/triggerDialogue.cs
+++ GoldMobile/Assets/Script/UI/triggerDialogue.cs
@@ -7,7 +7,7 @@ public class triggerDialogue : MonoBehaviour
     public DialogueData dialGhost;
     public DisplayText tdialogue;
     private bool firstTime;
-
+    public bool horse;
     // Start is called before the first frame update
     void Awake()
     {
@@ -17,15 +17,22 @@ public class triggerDialogue : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")){
-                
-            if (tdialogue.DoneTalking && !firstTime)
-            {
-                tdialogue.DialPass(dialGhost);
-                firstTime = true;
-            }
-            try
+            if (horse)
             {
                 GetComponent<Animator>().SetTrigger("horsed");
+
+            }
+            else
+            {
+                if (tdialogue.DoneTalking && !firstTime)
+                {
+                    tdialogue.DialPass(dialGhost);
+                    firstTime = true;
+                }
+            }
+            
+            try
+            {
             }
             catch { }
         }
