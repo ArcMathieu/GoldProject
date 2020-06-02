6f2eb260dfe65d2071e6be55c5cbb7e092be8ed2
Mathieu <mathieu.prevot.91@gmail.com>
2020-06-02 12:38:01 +0200

    Light && sprites

---
+++ GoldMobile/Assets/Lamp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Lamp : MonoBehaviour
+{
+    bool lightOn;
+
+    public void setAction()
+    {
+        if (lightOn)
+            lightOn = false;
+        else if (!lightOn)
+            lightOn = true;
+    }
+
+    private void Update()
+    {
+        if (lightOn)
+        {
+            transform.GetChild(0).GetComponent<Light2D>().intensity = Mathf.Lerp(transform.GetChild(0).GetComponent<Light2D>().intensity, 0, 0.2f);
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<Light2D>().intensity = Mathf.Lerp(transform.GetChild(0).GetComponent<Light2D>().intensity, 1.87f, 0.05f);
+        }
+    }
+}
